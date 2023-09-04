27d0532420fee1d7d3dc98fa25e4de4a755496b5
gregory3471 <144032712+gregory3471@users.noreply.github.com>
2023-09-04 16:44:27 +0300

    Create ;gregory

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @gregory3471
+- 👀 I’m interested in .coding and programming..
+- 🌱 I’m currently learning coding through software engineering...
+- 💞️ I’m looking to collaborate on programming adn coding of applications that can be of use to the community and the outside world...
+- 📫 How to reach me ;email me at kipngenogregory@gmail.com ; my official contact is;0748163492 ...
+
+<!---
+gregory3471/gregory3471 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
